a3ff6fb75d15a608c813d0a6a7e867da4e3a51ee
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-15 22:26:51 -0300

    Classe Controle Remoto e Interface Controlador
    Encapsulamento em Java, implementando o primeiro pilar da POO.

---
+++ Curso Poo Java/src/Aula06/Aula06.java
@@ -0,0 +1,10 @@
+package Aula06;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+        controle.ligar();
+        controle.ligar();
+        controle.abrirMenu();
+    }
+}

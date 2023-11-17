4b25157bdc8de0aecc5f184375e749f413bc2141
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-17 11:16:34 -0300

    Aula 03  atualizada
    Modifiquei o código criei uma interface Jogador de futebol e emplementei na classe Jogador

--- Curso Poo Java/src/Aula03/Jogador.java
+++ Curso Poo Java/src/Aula03/Jogador.java
@@ -1,6 +1,6 @@
 package Aula03;
 
-public class Jogador {
+public class Jogador implements JogadorFutebol{
     private String nome;
     private String nacionalidade;
     private String posição;
@@ -110,40 +110,42 @@ public class Jogador {
        this.anonascimento = anonascimento;
    }
 
-   void status(){
-       System.out.println("O nome desse jogador é: " + this.getNome());
-       System.out.println("A posição do seu jogador é: " + this.getPosição());
-       System.out.println("O Time por onde o seu jogador atua é o : " + this.getClube());
-       System.out.println("A quantidade de gols marcados pelo seu jogador até o momento é de: " +  this.getGols());
-       System.out.println("Os titulos vencidos por seu jogador são: " + this.getTitulos());
-   }
+@Override
+public void status() {
+    System.out.println("O nome desse jogador é: " + this.getNome());
+    System.out.println("A posição do seu jogador é: " + this.getPosição());
+    System.out.println("O Time por onde o seu jogador atua é o : " + this.getClube());
+    System.out.println("A quantidade de gols marcados pelo seu jogador até o momento é de: " +  this.getGols());
+    System.out.println("Os titulos vencidos por seu jogador são: " + this.getTitulos());
+}
 
-   public void jogar() {
-       System.out.println(getNome() + " está jogando!");
-   }
+@Override
+public void jogar() {
+    System.out.println(getNome() + " está jogando!");
+}
 
-   public void atacar(String alvo) {
-       System.out.println(getNome() + " está atacando " + getAlvo() + "!");
-   }
+@Override
+public void atacar() {
+    System.out.println(getNome() + " está atacando " + getAlvo() + "!");
+}
 
-   public void defender() {
-       System.out.println(getNome() + " está se defendendo!");
-   }
+@Override
+public void defender() {
+    System.out.println(getNome() + " está se defendendo!");
+}
 
-   public void obterInformacoes() {
-       System.out.println("Jogador: " + getNome() + ", Os seus títulos são " + getTitulos() + ", e os gols marcados foram" + getGols()); 
-   }
+@Override
+public void obterInformacoes() {
+    System.out.println("Jogador: " + getNome() + ", Os seus títulos são " + getTitulos() + ", e os gols marcados foram" + getGols()); 
+}
 
-   public void aumentarGols() {
-       this.gols++;
-   }
+@Override
+public void aumentarGols() {
+   this.gols ++;
+}
 
-   public void atualizarPontuacao(int pontos) {
-       this.gols += pontos;
-   }
 
-   public void alterarNome(String novoNome) {
-       this.nome = novoNome;
-   }
+
+   
 
 }
---
+++ Curso Poo Java/src/Aula03/JogadorFutebol.java
@@ -0,0 +1,10 @@
+package Aula03;
+
+public interface JogadorFutebol {
+    public abstract void status();
+    public abstract void jogar();
+    public abstract void atacar();
+    public abstract void defender();
+    public abstract void obterInformacoes();
+    public abstract void aumentarGols();
+}

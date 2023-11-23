604d69301049e78f23b5eecf8174ce984d0a720f
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-23 12:02:08 -0300

    Leitura de Dados
    Entrada de dados com o uso da Classe Scanner

---
+++ Curso Poo Java/src/Java Básico/Variáveis/SequenciaEscape.java
@@ -0,0 +1,8 @@
+public class SequenciaEscape {
+   public static void main(String[] args) {
+    // "Hello World!"
+    System.out.println("\"Hello, World!\"\n\r");
+    // 1/4
+    System.out.println("1\4");
+   }
+}

25f81c5adcbb48b8c9483dbbf538f0502a88013e
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-24 07:47:04 -0300

    If Else
    Condições If Elese

---
+++ Curso Poo Java/src/Java Básico/Condicionais If-Else/IfElse.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int idade = scanner.nextInt();
+
+        if(idade >= 18){
+            System.out.println("É maior de idade.");
+        } else{
+            System.out.println("Não é maior de idade");
+        }
+
+        //barato <= 10
+        //10 <  valor < 15 -  pedir desconto
+        //15 <= valor 17 -  pesquisar mais
+        // >= 17 - muito caro
+        System.out.println("Entre com um valor");
+        double valor = scanner.nextDouble();
+
+        if(valor <= 10){
+            System.out.println("Está barato, pode comprar");
+        } else if (valor < 10 && valor < 15){
+            System.out.println("Você pode pedir um desconto");
+        } else if(valor >= 15 && valor < 17){
+            System.out.println("Pode pesquisar mais");
+        } else{ // valor >= 17
+            System.out.println("Muito caro!");
+        }
+    }
+}

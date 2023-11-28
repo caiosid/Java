0c055bf5947a41e7787a3985e0aab4fe2e531feb
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-28 08:35:11 -0300

    .
    .

--- Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer07.java
+++ Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer07.java
@@ -1,7 +1,32 @@
-
+import java.util.Scanner;
 
 public class Exer07 {
     public static void main(String[] args) {
-        
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre com o primeiro número:");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Entre com um segundo número:");
+        int numero2 = scanner.nextInt();
+
+        System.out.println("Entre com o terceiro e último número:");
+        int numero3 = scanner.nextInt();
+
+        if (numero1 > numero2 && numero1 > numero3) {
+            System.out.println("Número 1 é maior: " + numero1);
+        } else if (numero2 > numero1 && numero2 > numero3) {
+            System.out.println("Número 2 é maior: " + numero2);
+        } else if (numero3 > numero1 && numero3 > numero2) {
+            System.out.println("Número 3 é o maior: " + numero3);
+        }
+
+        if (numero1 <= numero2 && numero1 <= numero3) {
+            System.out.println("Número 1 é maior: " + numero1);
+        } else if (numero2 <= numero1 && numero2 <= numero3) {
+            System.out.println("Número 2 é maior: " + numero2);
+        } else if (numero3 <= numero1 && numero3 <= numero2) {
+            System.out.println("Número 3 é o maior: " + numero3);
+        }
     }
 }

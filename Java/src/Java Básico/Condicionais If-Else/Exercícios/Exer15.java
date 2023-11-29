6998659d18f5c9e8a12fc0eb9fa5a60ca4cec574
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-29 07:48:31 -0300

    .
    .

--- Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer15.java
+++ Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer15.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Exer15 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Entre com o valro de a: ");
+       int a = scanner.nextInt();
+
+       if (a == 0) {
+          System.out.println("Não é equação de segundo grau");
+       } else{
+         System.out.println("Entte com ovalor de b:");
+       int b = scanner.nextInt();
+
+       System.out.println("Entre com o valor de c:");
+       int c =  scanner.nextInt();
+
+       double delta = (b+b) - (4*a*c);
+       if (delta < 0) {
+        System.out.println("delta negativo");
+       } else{
+
+        System.out.println("delta" +  delta);
+
+         double x1 = (-b) + Math.sqrt((delta)) / (2*a);
+         double x2 =  ((-b) + Math.sqrt(delta)) / (2*a);
+
+         System.out.println("x1 = " + x1);
+         System.out.println("x2 = " + 
+         x2);
+       }
+       }
+
+
+      
     }
 }
---
+++ Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer18.java
@@ -0,0 +1,5 @@
+public class Exer18 {
+    public static void main(String[] args) {
+        
+    }
+}

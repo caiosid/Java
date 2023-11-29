5404b29a2097e0d85988ae4e3cd47a97df6b7605
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-29 08:02:28 -0300

    .
    .

--- Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer19.java
+++ Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer19.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Exer19 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre com o primeiro número");
+        int primeiroNumero = scanner.nextInt();
+
+        System.out.println("Entre com o segundo número");
+        int segundoNumero = scanner.nextInt();
+
+        System.out.println("Entre com a operação (+ / * -)");
+        String operacao = scanner.next();
+
+        double resultado = 0;
+        boolean valida = true;
+
+        switch (operacao) {
+            case "+": resultado = primeiroNumero + segundoNumero; break;
+            case "-": resultado = primeiroNumero - segundoNumero; break;
+            case "*": resultado = primeiroNumero * segundoNumero; break;
+            case "/": resultado = primeiroNumero / segundoNumero; break;
+            default: System.out.println("Operação inválida");
+            valida = false;
+        }
+
+        if (valida) {
+            System.out.println("Resultado: " +  resultado);
+            if (resultado >=0) {
+                System.out.println("Resultado positivo");
+            } else{
+                System.out.println("resultado negativo");
+            }
+            if (resultado % 2 == 0) {
+                System.out.println("resultado par");
+            } else{
+                System.out.println("resultado impar");
+            }
+        } 
     }
 }

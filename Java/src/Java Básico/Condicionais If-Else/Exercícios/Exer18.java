e81badaaece05679a9357b414a36242400fa3ea7
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-29 07:51:18 -0300

    .
    .

--- Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer18.java
+++ Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer18.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Exer18 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre com um número");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("par");
+        } else{
+            System.out.println("impar");
+        }
     }
 }

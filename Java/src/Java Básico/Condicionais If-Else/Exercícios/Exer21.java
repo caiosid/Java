8ed3c98eba1e65ef27c1735c760197e5f6b81955
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-30 09:35:08 -0300

    .
    .

--- Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer21.java
+++ Java/src/Java Básico/Condicionais If-Else/Exercícios/Exer21.java
@@ -4,5 +4,42 @@ public class Exer21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        System.out.println("Entre com a quantidade de litros vendidos");
+        double litros = scanner.nextDouble();
+
+        System.out.println("Entre com o tipode combustível ");
+        String tipo = scanner.next();
+
+        double precoGas = 2.5;
+        double precoAlc = 1.9;
+        int percDesconto = 0;
+        double total = 0;
+        double totalDes = 0;
+
+        if (tipo.equalsIgnoreCase("a")) {
+            if (litros <= 20) {
+                percDesconto = 3;
+            } else{
+                percDesconto = 5;
+            }
+
+            total = litros * precoAlc;
+
+        } else if (tipo.equalsIgnoreCase("g")) {
+            if (litros <= 20) {
+                percDesconto = 4;
+            } else{
+                percDesconto = 6;
+            }
+
+            total = litros * precoGas;
+        }
+        
+        totalDes = (total / 100) * percDesconto;
+
+        double precoApagar = total - totalDes;
+
+        System.out.println("Valor a ser pago: " + precoApagar);
+        
     }
 }

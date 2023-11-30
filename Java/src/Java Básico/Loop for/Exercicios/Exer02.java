78892de6eaec565a360be4f06559cadd3818f8c5
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-30 11:05:56 -0300

    .
    .

--- Java/src/Java Básico/Loop for/Exercicios/Exer02.java
+++ Java/src/Java Básico/Loop for/Exercicios/Exer02.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
 
 public class Exer02 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean inforValidas = false;
+
+        do {
+            System.out.println("Entre com o nome do usuário");
+            String nomeUser = scanner.next();
+
+            System.out.println("Entre com a senha:");
+            String senha = scanner.next();
+
+            if (nomeUser.equalsIgnoreCase(senha)) {
+                // inforValidas = false;
+                System.out.println("Senha igual ao nome do usuário, digite novamente");
+            } else{
+                inforValidas = true;
+                System.out.println("Senha e usuários válidos!");
+            }
+
+        } while (!inforValidas);
         
     }
 }

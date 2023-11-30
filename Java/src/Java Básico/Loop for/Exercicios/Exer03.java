4365ecd0140ac14f7a4d1d126b932a94a5c366b4
caiocesar19 <111658365+caiocesar29@users.noreply.github.com>
2023-11-30 13:42:46 -0300

    .
    .

--- Java/src/Java Básico/Loop for/Exercicios/Exer03.java
+++ Java/src/Java Básico/Loop for/Exercicios/Exer03.java
@@ -1,4 +1,12 @@
 import java.util.Scanner;
+/*
+ * Faça um programa que leia e valide as seguintes informações:
+   a. Nome: maior que 3 caracteres;
+   b. Idade: entre 0 e 150; 
+   c. Salário maior que zero;
+   d. Sexo "f" ou "m";   
+   e. Estado Civil 'solteiro', 'casado', 'diforciado', 'viuvo'
+ */
 
 public class Exer03 {
     public static void main(String[] args) {
@@ -6,7 +14,7 @@ public class Exer03 {
         Scanner scanner = new Scanner(System.in);
 
         boolean infoValida = true;
-        String nome, sexo;
+        String nome, sexo, estadoCivil;
         int idade;
         double salario;
 
@@ -48,11 +56,29 @@ public class Exer03 {
         do {
             System.out.println("Entre com o seu gênero");
             sexo = scanner.next();
-            if (infoValida) {
-                
+            if (sexo.equalsIgnoreCase("femenino") || sexo.equalsIgnoreCase("masculino")) {
+                infoValida =  true;
+                System.out.println("Sexo precisa ser 'f' ou 'm'");
             }
             
-        } while (infoValida);
+        } while (!infoValida);
 
+        infoValida = false;
+        do {
+            System.out.println("Entre com o estado civil: ");
+            estadoCivil = scanner.next();
+            if (estadoCivil.equalsIgnoreCase("solteiro") || estadoCivil.equalsIgnoreCase("casado") || estadoCivil.equalsIgnoreCase("divorciado") || estadoCivil.equalsIgnoreCase("viuvo")) {
+                infoValida = true;
+            } else{
+                System.out.println("Estado civil precisa ser 'solteiro', 'casado','divorvciado', 'viuvo'");
+            }
+        } while (!infoValida);
+        
+        System.out.println("As seguintes informações foram coletadas:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Estado Civil: " + estadoCivil);
     }
 }
--- Java/src/Java Básico/Loop for/Exercicios/Exer05.java
+++ Java/src/Java Básico/Loop for/Exercicios/Exer05.java
@@ -1,7 +1,70 @@
-
+import java.util.Scanner;
 
 public class Exer05 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double populacaoA;
+        double populacaoB;
+        double taxaA;
+        double taxaB;
+
+         boolean valido = false;
+        do {
+            System.out.println("Entre com a população A: ");
+            populacaoA = scanner.nextDouble();
+            if (populacaoA > 0) {
+                valido = true;
+            } else{
+                System.out.println("População A precisa ser maior que 0");
+            }
+        } while (!valido);
+
+        valido = false;
+        do {
+            System.out.println("Entre com a população B: ");
+            populacaoB = scanner.nextDouble();
+            if (populacaoB > 0) {
+                valido = true;
+            } else{
+                System.out.println("População B precisa ser maior que 0");
+            }
+        } while (!valido);
+
+        valido = false;
+        do {
+            System.out.println("Entre com a taxa de crescimento da população A:");
+            taxaA = scanner.nextDouble();
+            if (taxaA > 0) {
+                valido = true;
+            } else{
+                System.out.println("Taxa de crescimento A precisa ser maior que 0 ");
+            }
+        } while (!valido);
+
+        valido = false;
+        do {
+            System.out.println("Entre com a taxa de crescimento da populção B: ");
+            taxaB = scanner.nextDouble();
+            if (taxaB > 0) {
+                System.out.println("Taxa de crescumento B precisa ser maior do que 0");
+            }
+        } while (!valido);
+
+        int cont = 0;
+        while (populacaoA < populacaoB) {
+            populacaoA += (populacaoA/100) * taxaA;
+            populacaoB += (populacaoB/100) * taxaB;
+            cont ++;
+        }
+
+        System.out.println("População A: " + populacaoA);
+        System.out.println("População B:" + populacaoB);
+        System.out.println("Quantidade de anos: " + cont);
+
+        
+
         
     }
 }
